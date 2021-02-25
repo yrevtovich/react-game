335ff98642ad512369b3a7825c3768ac3169805f
Yuliya Reutovich <yrevtovich@mail.ru>
2021-02-26 01:50:13 +0300

    refactor: add eslint airbnb config

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base', 
+    'airbnb', 
+    'airbnb/hooks', 
+    'airbnb-typescript',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'linebreak-style': 0,
+    'global-require': 0,
+    'eslint linebreak-style': [0, 'error', 'windows'],
+  },
+};
